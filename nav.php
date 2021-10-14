d2aa4c0b6ba03698b1ba481c93dfd76218c45d73
EmmanuelPng <emanpng@gmail.com>
2021-10-14 15:54:16 +0800

    First Update
    Inserted 7 files

---
+++ nav.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<div id="banner"><img src="Images/PoliceBanner.jpg"></div>
+<div class="topnav">
+ <a href="logcall.php">Log Call</a>
+ <a href="Update.php">Update</a>
+ <a href="#">Report</a>
+ <a href="#">History</a>
+	</div>
+	
+	<!--   <nav>
+ <a href="logcall.php">Log Call</a>
+ <a href="Update.php">Update</a>
+ <a href="#">Report</a>
+ <a href="#">History</a>
+ </nav>   -->
+	
+</head>
+
+<body>
+</body>
+</html>
